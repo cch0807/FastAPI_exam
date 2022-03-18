0cf4d0b74ef99af9a7de8d344aafff37a1bc7aff
cch0807 <https://github.com/cch0807>
2022-03-18 11:05:06 +0900

    Modify segment

--- app/handler/rest/v1/segment/dtos.py
+++ app/handler/rest/v1/segment/dtos.py
@@ -1,57 +1,66 @@
 from datetime import datetime
-from pydoc import describe
-from typing import Optional, List, json
+from pydantic import Field
+from typing import Optional, List
 
 from app.handler.rest.v1._base import BaseDTO
-from app.common.enum.dataset import DatasetStatus
+
 
 class SegmentInputDTO(BaseDTO):
-  name: Optional[str]
-  description: Optional[str]
-  # api_idx: int
-  # dataset_idx: int
-  status: str
+    name: Optional[str]
+    description: Optional[str]
+    # api_idx: int
+    # dataset_idx: int
+    status: str
+
 
 class ParameterInputDTO(BaseDTO):
-  name: str
-  description: Optional[str]
-  type: str
-  formula: dict
+    name: str
+    description: Optional[str]
+    type: str
+    formula: dict
+
 
 class SegmentCreateDTO(BaseDTO):
-  name: str
-  description: Optional[str]
-  type: str
-  prameters: List[ParameterInputDTO]
+    name: str
+    description: Optional[str]
+    type: str
+    prameters: List[ParameterInputDTO]
+
 
 class ParameterResponseDTO(BaseDTO):
-  idx: int
-  name: str
-  type: str
-  formula: dict
+    id: int = Field(alias="idx")
+    name: str
+    type: str
+    formula: dict
+
 
 class SegmentResponseDTO(BaseDTO):
-  idx: int
-  name: str
-  # api_name: str
-  # dataset_name: str
-  parameter: List[ParameterResponseDTO]
-  region_count: int
-  # creater: str
-  # checker: str
-  created_date: datetime
-  modified_date: datetime
-  # Checked_date: datetime
-  status: str
+    id: int = Field(alias="idx")
+    name: str
+    # api_name: str
+    # dataset_name: str
+    parameter: List[ParameterResponseDTO]
+    region_count: int
+    # creater: str
+    # checker: str
+    created_date: datetime
+    modified_date: datetime
+    # Checked_date: datetime
+    status: str
+
 
 class DatasetFieldsReponseDTO(BaseDTO):
     idx: int
     name: str
     description: str
 
-class DatasetResponeDTO(BaseDTO):
+
+class DatasetResponseDTO(BaseDTO):
     idx: int
     name: str
     description: Optional[str]
     fields: List[DatasetFieldsReponseDTO]
 
+
+class DatasetDetailResponseDTO(DatasetResponseDTO):
+    fields: List[DatasetFieldsReponseDTO]
